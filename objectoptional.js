d3322c562f623da7d28c5f366dd10faa9589188b
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-04-01 07:56:54 +0545

    Learning about optional object property

---
+++ objectoptional.js
@@ -0,0 +1,9 @@
+var emp1 = {
+    name: "jyoti"
+};
+console.log(emp1);
+var emp2 = {
+    name: "Pinku",
+    age: 20
+};
+console.log(emp2);
---
+++ objectoptional.ts
@@ -0,0 +1,14 @@
+type Employee={
+     name:string;
+     age?:number;
+}
+
+ let emp1:Employee={
+     name:"jyoti"
+ }
+  console.log(emp1);
+   let emp2:Employee={
+     name:"Pinku",
+     age:20
+   };
+   console.log(emp2);
