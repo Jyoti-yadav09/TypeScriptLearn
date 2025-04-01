380a319c39e351c8b9023f0aeb28a8380dfede92
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-04-01 08:23:12 +0545

    Learning about Readonly properties in objects

---
+++ Readonlyobject.js
@@ -0,0 +1,5 @@
+var car1 = {
+    name: "BMW",
+    model: 2023
+};
+console.log(car1);
---
+++ Readonlyobject.ts
@@ -0,0 +1,11 @@
+type car={
+    readonly name:string;
+     readonly model:number;
+}
+
+ let car1:car={
+     name:"BMW",
+     model:2023
+ };
+  
+  console.log(car1);
