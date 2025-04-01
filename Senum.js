941f63c5d7a5715d685f03d5c9aa8d9911daf3ea
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-04-01 15:21:55 +0545

    Learning about String enum

---
+++ Senum.js
@@ -0,0 +1,12 @@
+var Direction;
+(function (Direction) {
+    Direction["up"] = "UP";
+    Direction["left"] = "LEFT";
+    Direction["right"] = "RIGHT";
+    Direction["down"] = "DOWN";
+})(Direction || (Direction = {}));
+;
+console.log(Direction.up);
+console.log(Direction.left);
+console.log(Direction.down);
+console.log(Direction.right);
---
+++ Senum.ts
@@ -0,0 +1,12 @@
+enum Direction
+{
+     up="UP",
+     left="LEFT",
+     right="RIGHT",
+     down="DOWN"
+};
+
+ console.log(Direction.up);
+ console.log(Direction.left);
+ console.log(Direction.down);
+ console.log(Direction.right);
