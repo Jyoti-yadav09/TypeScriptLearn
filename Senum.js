0b315b28f7d7a6a3683007ca45946ee27e9b84af
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-04-01 15:37:05 +0545

    Learning about how to use enums in function

--- Senum.js
+++ Senum.js
@@ -5,8 +5,25 @@ var Direction;
     Direction["right"] = "RIGHT";
     Direction["down"] = "DOWN";
 })(Direction || (Direction = {}));
-;
 console.log(Direction.up);
 console.log(Direction.left);
 console.log(Direction.down);
 console.log(Direction.right);
+/*Uing enums in function*/
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["User"] = 1] = "User";
+    Role[Role["Guest"] = 2] = "Guest";
+})(Role || (Role = {}));
+;
+function checkAccess(role) {
+    if (role === Role.Admin) {
+        console.log("Access Granted");
+    }
+    else {
+        console.log("Access denied");
+    }
+}
+checkAccess(Role.Admin);
+checkAccess(Role.Guest);
--- Senum.ts
+++ Senum.ts
@@ -4,9 +4,29 @@ enum Direction
      left="LEFT",
      right="RIGHT",
      down="DOWN"
-};
+}
 
  console.log(Direction.up);
  console.log(Direction.left);
  console.log(Direction.down);
  console.log(Direction.right);
+
+ /*Uing enums in function*/
+ enum Role{
+      Admin,
+      User, 
+      Guest
+ };
+
+  function checkAccess(role:Role)
+  {
+      if(role===Role.Admin)
+      {
+           console.log("Access Granted");
+      }
+      else{
+           console.log("Access denied");
+      }
+  }
+   checkAccess(Role.Admin);
+   checkAccess(Role.Guest);
